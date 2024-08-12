69616cb097534be5fdddd6a72d297b6b42c69284
jha.kuldip <jhakuldip40@gmail.com>
2024-08-12 15:08:02 +0530

    Added myfirstmodule Modules

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
 const http = require('http');
+var dt = require('./myfirstmodule');
 
 // Define the port and hostname
 const PORT = 3000;
@@ -9,6 +10,8 @@ const HOSTNAME = '127.0.0.1';
 const server = http.createServer((req, res) => {
   res.statusCode = 200; // Set the HTTP status code
   res.setHeader('Content-Type', 'text/plain'); // Set the content type
+  //res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write("The date and time are currently: " + dt.myDateTime());
   res.end('Hello, world!\n'); // Send the response
 });
 
